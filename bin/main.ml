687eefca665074e800e88d614cf823f9fa87c126
Aram Brouwer <aramvanderpols05@gmail.com>
2025-05-19 22:32:20 +0200

    Splitted up the fwd function in 2 seperate functions and implemented the 2 error functions

--- bin/main.ml
+++ bin/main.ml
@@ -7,26 +7,38 @@ let append_bias_col a: Mat.mat =
   let col = Mat.create n_rows 1 (-1.) in
   Mat.concat_horizontal a col
 
-let fwd (inputs: Mat.mat) (weights1: Mat.mat) (weights2: Mat.mat) (beta: float)=
+let hidden_layers_activations (inputs: Mat.mat) (weights1: Mat.mat) (beta: float): Mat.mat =
   let hidden =  Mat.(inputs *@ weights1) in
   let hidden' = append_bias_col (Mat.map (fun x -> activation_func x beta) hidden) in
-  let outputs = Mat.(hidden' *@ weights2) in
-  Mat.print hidden';
-  Mat.print outputs
+  hidden'
+
+let output_layer_activations (hidden: Mat.mat) (weights2: Mat.mat) (beta: float): Mat.mat =
+  let outputs = Mat.(hidden *@ weights2) in
+  let outputs' = Mat.map (fun x -> activation_func x beta) outputs in
+  outputs'
 
 let train (inputs: Mat.mat) (targets: Mat.mat) (*(eta: float) (niterations: int)*) (nhidden: int) =  
   let nIn = Mat.col_num inputs in
   (*let nData = Mat.row_num inputs in*)
   let nOut = Mat.row_num targets in
   let inputs' = append_bias_col inputs in
-  Mat.print inputs';
   
   (*initialize weight matrices to small random values*)
   let weights1 = Mat.map (( *. ) (2. /. sqrt((float) nIn))) (Mat.map ((-.) 0.5) (Mat.uniform (nIn+1) nhidden)) in
   let weights2 = Mat.map (( *. ) (2. /. sqrt((float) nIn))) (Mat.map ((-.) 0.5) (Mat.uniform (nhidden+1) nOut)) in
   
-  fwd inputs' weights1 weights2 1.
+  (*Forwards fase*)
+  let hidden = hidden_layers_activations inputs' weights1 1. in
+  let outputs = output_layer_activations hidden weights2 1. in
 
+  (*compute error at the output*)
+  let deltao = Mat.((outputs - targets) *@ outputs *@ (create (row_num outputs) (col_num outputs) (-1.) - outputs)) in
+  (* compute error at the hidden layers *)
+  let deltah = Mat.( hidden *@ (create (row_num hidden) (col_num hidden) (-1.) - outputs) *@ (deltao *@ (transpose weights2))) in
+
+  Mat.print outputs;
+  Mat.print deltao
+  
 
 let () =
   (* AND data*)
@@ -40,7 +52,5 @@ let () =
   let anddata = Mat.of_arrays anddata_arr in
   let and_input = Owl.Mat.cols anddata [|0;1|] in
   let and_targets = Owl.Mat.col anddata 2 in
-  let nhidden = 2 in
-
+  let nhidden = 2 in  
   train and_input and_targets nhidden
-  
